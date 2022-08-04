99c0bddca1000f0a22fb6fc1532591a2e799472e
Vahidin <vhasic1@etf.unsa.ba>
2022-08-04 11:58:19 +0200

    Finished primary navigation

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+const primaryHeader = document.querySelector(".primary-header");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNav = document.querySelector(".primary-navigation");
 
+/* On mobile screens when hamburger icon is clicked show or hide nav list */
+navToggle.addEventListener('click', ()=>{
+    // primaryNav.classList.toggle('opened');
+
+    primaryNav.hasAttribute('data-visible') ? navToggle.setAttribute('aria-expanded', false) : navToggle.setAttribute('aria-expanded', true) ;
+    primaryNav.toggleAttribute('data-visible');
+    primaryHeader.toggleAttribute('data-overlay');
+})
