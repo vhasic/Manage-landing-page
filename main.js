6d1fbdcfa6b1eb71cff4c1bedccf93783c8c936d
Vahidin <vhasic1@etf.unsa.ba>
2022-08-06 15:50:32 +0200

    Styling footer

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const primaryHeader = document.querySelector(".primary-header");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-navigation");
+const iconHamburger = document.querySelector(".icon-hamburger");
+const iconClose = document.querySelector(".icon-close");
 
 /* On mobile screens when hamburger icon is clicked show or hide nav list */
 navToggle.addEventListener('click', ()=>{
@@ -9,4 +11,11 @@ navToggle.addEventListener('click', ()=>{
     primaryNav.hasAttribute('data-visible') ? navToggle.setAttribute('aria-expanded', false) : navToggle.setAttribute('aria-expanded', true) ;
     primaryNav.toggleAttribute('data-visible');
     primaryHeader.toggleAttribute('data-overlay');
+    if (iconHamburger.style.display === "none"){
+        iconHamburger.style.display="block"
+        iconClose.style.display="none"
+    } else {
+        iconHamburger.style.display="none"
+        iconClose.style.display="block" 
+    }
 })
