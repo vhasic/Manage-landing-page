eb6a8001e9317ae11b90f28aab336e0cd3b8c722
Vahidin <vhasic1@etf.unsa.ba>
2022-08-24 11:22:04 +0200

    Added background images

--- main.js
+++ main.js
@@ -5,30 +5,32 @@ const iconHamburger = document.querySelector(".icon-hamburger");
 const iconClose = document.querySelector(".icon-close");
 
 /* On mobile screens when hamburger icon is clicked show or hide nav list */
-navToggle.addEventListener('click', ()=>{
-    // primaryNav.classList.toggle('opened');
+navToggle.addEventListener("click", () => {
+  // primaryNav.classList.toggle('opened');
 
-    primaryNav.hasAttribute('data-visible') ? navToggle.setAttribute('aria-expanded', false) : navToggle.setAttribute('aria-expanded', true) ;
-    primaryNav.toggleAttribute('data-visible');
-    primaryHeader.toggleAttribute('data-overlay');
-    if (iconHamburger.style.display === "none"){
-        iconHamburger.style.display="block"
-        iconClose.style.display="none"
-    } else {
-        iconHamburger.style.display="none"
-        iconClose.style.display="block" 
-    }
-})
+  primaryNav.hasAttribute("data-visible")
+    ? navToggle.setAttribute("aria-expanded", false)
+    : navToggle.setAttribute("aria-expanded", true);
+  primaryNav.toggleAttribute("data-visible");
+  primaryHeader.toggleAttribute("data-overlay");
+  if (iconHamburger.style.display === "none") {
+    iconHamburger.style.display = "block";
+    iconClose.style.display = "none";
+  } else {
+    iconHamburger.style.display = "none";
+    iconClose.style.display = "block";
+  }
+});
 
-// to rotate images in slider 
-const slider = new A11YSlider(document.querySelector('.slider'), {
-    adaptiveHeight: false,
-    dots: true,
-    centerMode: true,
-    arrows: false, // removing buttons previous/next slide
-    responsive: {
-        480: {
-          dots: false // dots disabled 480px and up
-        }
-      }
-  });
+// to rotate images in slider
+const slider = new A11YSlider(document.querySelector(".slider"), {
+  adaptiveHeight: false,
+  dots: true,
+  centerMode: true,
+  arrows: false, // removing buttons previous/next slide
+  responsive: {
+    480: {
+      dots: false, // dots disabled 480px and up
+    },
+  },
+});
